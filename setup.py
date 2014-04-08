72cc2ce89812a78739f829fc9752f138abebe209
Kosta Harlan <kostajh@gmail.com>
2014-04-08 10:29:28 -0400

    Add MANIFEST.in, and ignore MANIFEST. Bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from distutils.core import setup
 
 setup(
   name='pyac',
-  version='0.1.2',
+  version='0.1.3',
   author='Kosta Harlan',
   author_email='kosta@harlan.mayfirst.org',
   url='https://github.com/kostajh/pyac',
   packages=['pyac'],
-  license='LICENSE.txt',
   description='A library for activeCollab 3.x/4.x',
   long_description=open('README.md').read(),
 )
