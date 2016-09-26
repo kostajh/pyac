21b084d119b2a336b3e3319e37d0c0cfeb80cc64
Kosta Harlan <kosta@kostaharlan.net>
2016-09-26 10:10:17 -0400

    Update version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(
   name='pyac',
-  version='0.1.3',
+  version='0.1.5',
   author='Kosta Harlan',
-  author_email='kosta@harlan.mayfirst.org',
+  author_email='kosta@kostaharlan.net',
   url='https://github.com/kostajh/pyac',
   packages=['pyac'],
   description='A library for activeCollab 3.x/4.x',
